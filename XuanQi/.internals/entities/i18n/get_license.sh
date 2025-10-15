bcd12581e01548f8bd471884f9632c465f1df64b
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-15 19:10:46 +0800

    XuanQi: added entities_i18n_get_license
    Since we need an internationalized 'license' label, we have
    to develop the function for it. Hence, let's do this.
    
    This patch adds entities_i18n_get_license into XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ XuanQi/.internals/entities/i18n/get_license.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+# Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 "Holloway" Chew, Kean Ho <kean.ho.chew@zoralab.com>
+# Copyright 2023 ZORALab Enterprise <tech@zoralab.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
+# You must comply with the license to use the content. Get the License at:
+#
+# https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+
+
+
+
+# Parameters:
+# Returns:
+#       String
+#               - the translated content.
+#       Return Code
+#               - '0' means ok; error otherwise.
+#               - error on bad execution.
+entities_i18n_get_license() {
+        # execute
+        case "$XUANQI_LANGUAGE" in
+        "de")
+                printf -- "%s" "Lizenz"
+                ;;
+        "es")
+                printf -- "%s" "Licencia"
+                ;;
+        "fr")
+                printf -- "%s" "Licence"
+                ;;
+        "ja")
+                printf -- "%s" "ライセンス"
+                ;;
+        "ko")
+                printf -- "%s" "라이선스"
+                ;;
+        "mn-Cyrl")
+                printf -- "%s" "Лиценз"
+                ;;
+        "ms")
+                printf -- "%s" "Lesen"
+                ;;
+        "nb")
+                printf -- "%s" "Lisens"
+                ;;
+        "nl")
+                printf -- "%s" "Licentie"
+                ;;
+        "nn")
+                printf -- "%s" "Lisens"
+                ;;
+        "ru")
+                printf -- "%s" "Лицензия"
+                ;;
+        "sv")
+                printf -- "%s" "Licens"
+                ;;
+        "uk")
+                printf -- "%s" "Ліцензія"
+                ;;
+        "zh-Hans")
+                printf -- "%s" "许可证"
+                ;;
+        "zh-Hant")
+                printf -- "%s" "許可證"
+                ;;
+        *)
+                # fallback to english
+                printf -- "%s" "License"
+                ;;
+        esac
+        if [ $? -ne 0 ]; then
+                return 1
+        fi
+
+
+        # report status
+        return 0
+}
+
+
+
+
+# report import status
+return 0
