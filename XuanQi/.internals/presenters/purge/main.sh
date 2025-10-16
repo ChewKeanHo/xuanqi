4d5d0bfedd6f85d2e5357e56fb610ba78eb8b7bb
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-16 14:39:36 +0800

    XuanQi: added presenters/purge/main.sh API
    Since we have all the purge API ready, we can proceed to
    develop its presenter subroutine. Hence, let's do this.
    
    This patch adds presenters/purge/main.sh API into XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ XuanQi/.internals/presenters/purge/main.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+# Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 "Holloway" Chew, Kean Ho <kean.ho.chew@zoralab.com>
+# Copyright 2023 ZORALab Enterprise <tech@zoralab.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
+# You must comply with the license to use the content. Get the License at:
+#
+# https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+
+
+
+
+# define all directory paths if unset
+XUANQI_DIRECTORY_LOGS="${XUANQI_DIRECTORY_LOGS:-"logs"}"
+XUANQI_DIRECTORY_PUBLIC="${XUANQI_DIRECTORY_PUBLIC:-"public"}"
+XUANQI_DIRECTORY_TEMPS="${XUANQI_DIRECTORY_TEMPS:-"tmp"}"
+
+
+
+
+# locate all internal system paths
+____old_IFS="$IFS"
+while IFS="" read -r ____line || [ -n "$____line" ]; do
+        if [ "$____line" = "" ]; then
+                continue
+        fi
+
+        if [ ! -e "${PROJECT_PATH_ROOT}/${____line#*": "}" ]; then
+                continue
+        elif [ -d "${PROJECT_PATH_ROOT}/${____line#*": "}" ]; then
+                interactors_print_info "\
+$(interactors_print_responses_removing "\$${____line%%": "*}")
+"
+                interactors_fs_remove "${PROJECT_PATH_ROOT}/${____line#*": "}"
+                continue
+        fi
+done<<EOF
+XUANQI_PATH_LOGS: ${XUANQI_DIRECTORY_LOGS}
+XUANQI_PATH_PUBLIC: ${XUANQI_DIRECTORY_PUBLIC}
+XUANQI_PATH_TEMPS: ${XUANQI_DIRECTORY_TEMPS}
+EOF
+
+
+
+
+# report status
+return 0
