9b4552a815f920659daf20c919e8877f42f8ffc0
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-16 09:05:35 +0800

    XuanQi: added presenters/help/main.sh
    Since the entire help API is ready, we can proceed to develop
    its main presenter API. Hence, let's do this.
    
    This patch adds presenters/help/main.sh into XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ XuanQi/.internals/presenters/help/main.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 "Holloway" Chew, Kean Ho <kean.ho.chew@zoralab.com>
+# Copyright 2023 ZORALab Enterprise <tech@zoralab.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
+# You must comply with the license to use the content. Get the License at:
+#
+# https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+
+
+
+
+# print trademark as header
+interactors_print_info "\
+$(interactors_print_brand_header)
+"
+interactors_print_info "$(interactors_print_ui_horizontal_line 3)"
+
+
+
+
+# run through each specific section
+case "$2" in
+*)
+        interactors_print_info "$(interactors_print_help_index)"
+        ;;
+esac
+
+
+
+
+# print footer
+interactors_print_info "$(interactors_print_ui_horizontal_line 3)"
+
+
+
+
+# report status
+return 0
