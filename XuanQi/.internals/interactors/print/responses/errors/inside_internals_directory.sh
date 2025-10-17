876560351b3c4953d64b000df9a09a52c2ec2d6d
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-17 11:00:55 +0800

    XuanQi: added interactors_print_responses_errors_inside_internals_directory
    Since the inside '.internals/' directory error response data
    source is ready, we can now enable its interactor function. Hence,
    let's do this.
    
    This patch adds
    interactors_print_responses_errors_inside_internals_directory
    into XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ XuanQi/.internals/interactors/print/responses/errors/inside_internals_directory.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+# Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 "Holloway" Chew, Kean Ho <kean.ho.chew@zoralab.com>
+# Copyright 2023 ZORALab Enterprise <tech@zoralab.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
+# You must comply with the license to use the content. Get the License at:
+#
+# https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+
+
+
+
+# Parameters:
+# Outputs:
+#       String
+#               - the string content.
+#               - empty on error.
+# Returns:
+#       Return Code
+#               - '0' means ok; error otherwise.
+#               - error on bad execution.
+interactors_print_responses_errors_inside_internals_directory() {
+        # execute
+        entities_i18n_errors_inside_internals_directory
+        if [ $? -ne 0 ]; then
+                return 1
+        fi
+
+
+        # report status
+        return 0
+}
+
+
+
+
+# report import status
+return 0
