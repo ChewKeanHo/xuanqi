542ac9374e24cc81cdfb611d3e30c0e434a51ddd
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-18 17:12:54 +0800

    XuanQi: added views_shell_get_indent
    Since we need to create shell script systematically, we have
    to develop the shell rendering function. Fortunately, we got
    Hestia Project to back us up so the porting should be relatively
    easy. Hence, let's do this.
    
    This patch adds views_shell_get_indent into XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ XuanQi/.internals/views/shell/get_indent.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+# Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
+# You must comply with the license to use the content. Get the License at:
+#
+# https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+
+
+
+
+# Parameters:
+#       ____input
+#               - OPTIONAL
+#               - Number type.
+#               - 0 returns empty indent.
+# Outputs:
+#       String
+#               - the rendered total indent count in string.
+#               - empty on error.
+# Returns:
+#       Return Code
+#               - '0' means ok; error otherwise.
+#               - error on invalid input (not a round number or <0).
+#               - error on bad execution.
+views_shell_get_indent() {
+        #____count="$1"
+
+
+        # execute
+        case "$1" in
+        ""|*[!0-9]*)
+                printf -- "%s" ""
+                return 1
+                ;;
+        *)
+                ____indent=""
+                ____count="$1"
+                while [ $____count -gt 0 ]; do
+                        ____indent="${____indent}        "
+                        ____count="$(( $____count - 1 ))"
+                done
+
+                printf -- "%s" "$____indent"
+                if [ $? -ne 0 ]; then
+                        unset ____indent ____count
+                        return 1
+                fi
+                unset ____indent ____count
+                ;;
+        esac
+
+
+        # report status
+        return 0
+}
+
+
+
+
+# report import status
+return 0
