ad0d466565f3f3f94f815f95d082e741291175d3
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-18 15:14:24 +0800

    XuanQi: added entities_time_format_second
    Since we need a reliable way to format a time for seconds in digit,
    we have to develop its function. Hence, let's do this.
    
    This patch adds entities_time_format_second in XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ XuanQi/.internals/entities/time/format_second.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+# Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 "Holloway" Chew, Kean Ho <kean.ho.chew@zoralab.com>
+# Copyright 2023 ZORALab Enterprise <tech@zoralab.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
+# You must comply with the license to use the content. Get the License at:
+#
+# https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+
+
+
+
+# Parameters:
+#       ____unix_epoch
+#               - COMPULSORY
+#               - The unix epoch (uint32/uint64) timestamp.
+#       ____timezone
+#               - OPTIONAL
+#               - The custom timezone adjustment.
+#               - Strictly in "+HHMM" or "-HHMM" format (e.g.'-0800' or '+0200')
+#               - When unset, '+0000' will be used as default UTC.
+# Outputs:
+#       String
+#               - the second in number.
+#               - Examples
+#                       "1760742707" + "+0230" = "47"
+# Returns:
+#       Return Code
+#               - '0' means ok; error otherwise.
+#               - error on bad execution.
+entities_time_format_second() {
+        #____unix_epoch="$1"
+        #____timezone="$2"
+
+
+        # execute
+        entities_time_format "%S" "$1" "$2"
+        if [ $? -ne 0 ]; then
+                printf -- ""
+                return 1
+        fi
+
+
+        # report status
+        return 0
+}
+
+
+
+
+# report import status
+return 0
