7ed4f75e259345e627758e44060b5c504d785532
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-18 16:21:27 +0800

    XuanQi: added entities_i18n_errors_locate_directory_configs
    It's better to unified all repsonses using function rather than
    reading individual files for responses. Hence, let's improve
    existing file-based responses.
    
    This patch adds entities_i18n_errors_locate_directory_configs
    to unify and to standardize error responses in XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

--- XuanQi/.internals/interactors/print/responses/errors/locate_directory_configs.sh
+++ XuanQi/.internals/interactors/print/responses/errors/locate_directory_configs.sh
@@ -27,25 +27,11 @@
 #               - '0' means ok; error otherwise.
 #               - error on bad execution.
 interactors_print_responses_errors_locate_directory_configs() {
-        # validate inputs
-        ____path="\
-${XUANQI_PATH_ROOT}/entities/docs/responses/errors/locate_directory_configs/${XUANQI_LANGUAGE}.txt\
-"
-        if [ ! -f "$____path" ]; then
-                # fallback to english
-                ____path="\
-${XUANQI_PATH_ROOT}/entities/docs/responses/errors/locate_directory_configs/en.txt\
-"
-        fi
-
-
         # execute
-        entities_fs_read_text_file "$____path"
+        entities_i18n_errors_locate_directory_configs
         if [ $? -ne 0 ]; then
-                unset ____path
                 return 1
         fi
-        unset ____path
 
 
         # report status
