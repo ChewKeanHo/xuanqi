8e3bb623db8644c0f18aa849ffe8de35f7ed96c1
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-19 13:47:41 +0800

    XuanQi: added views_shell_write_long_string_closer
    Since we need to create shell script systematically, we have
    to develop the shell rendering function. Fortunately, we got
    Hestia Project to back us up so the porting should be relatively
    easy. Hence, let's do this.
    
    This patch adds views_shell_write_long_string_closer into XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ XuanQi/.internals/views/shell/write_long_string_closer.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+# Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
+# You must comply with the license to use the content. Get the License at:
+#
+# https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+
+
+
+
+# Parameters:
+#       ____path_dest
+#               - COMPULSORY
+#               - The destination file to write into.
+# Outputs:
+#       Write to $____path_dest File
+#               - the rendered output written into file.
+#               - no action on error.
+# Returns:
+#       Return Code
+#               - '0' means ok; error otherwise.
+#               - error on invalid '$____path_dest' (e.g. 'empty').
+#               - error on bad execution.
+views_shell_write_long_string_closer() {
+        #____path_dest="$1"
+
+
+        # validate inputs
+        if [ "$1" = "" ]; then
+                return 1
+        fi
+
+
+        # execute
+        printf -- "%s" "\
+\"
+" >> "${1}.tmp"
+        if [ $? -ne 0 ]; then
+                return 1
+        fi
+
+
+        # report status
+        return 0
+}
+
+
+
+
+# report import status
+return 0
