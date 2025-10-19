37d2667e3cad4be7a10b9e93eb1c3da8b55a2b2a
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-19 12:41:27 +0800

    XuanQi: added views_shell_write_if_else_opener
    Since we need to create shell script systematically, we have
    to develop the shell rendering function. Fortunately, we got
    Hestia Project to back us up so the porting should be relatively
    easy. Hence, let's do this.
    
    This patch adds views_shell_write_if_else_opener into XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ XuanQi/.internals/views/shell/write_if_else_opener.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+# Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
+# You must comply with the license to use the content. Get the License at:
+#
+# https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+
+
+
+
+# Parameters:
+#       ____path_dest
+#               - COMPULSORY
+#               - The destination file to write into.
+#       ____condition
+#               - COMPULSORY
+#               - The 'if' condition type.
+#               - Supply '[ ]' since it's possible to have multi conditions.
+#               - Multi-lines 1 entry value. (e.g.
+#                 "\
+#                       [ "$1" = "" ] &&
+#                       [ "$2" = "..." ] &&
+#                       ...
+#                 ").
+#               - Manage the indent on your side. Remember to subtract from
+#                 $____indent.
+#       ____indent
+#               - OPTIONAL
+#               - The base indent level in round number.
+#               - When empty, the indent level will set to '0' (no indent).
+# Outputs:
+#       Write to $____path_dest File
+#               - the rendered output written into file.
+#               - no action on error.
+# Returns:
+#       Return Code
+#               - '0' means ok; error otherwise.
+#               - error on invalid '$____path_dest' (e.g. 'empty').
+#               - error on invalid '$____indent' (e.g. not a number).
+#               - error on bad execution.
+views_shell_write_if_else_opener() {
+        #____path_dest="$1"
+        #____condition="$2"
+        #____indent="$3"
+
+
+        # validate inputs
+        if [ "$1" = "" ]; then
+                return 1
+        fi
+
+        if [ "$2" = "" ]; then
+                return 1
+        fi
+
+
+        # execute
+        case "$3" in
+        "")
+                ____indent=0
+                ;;
+        *[!0-9]*)
+                unset ____indent
+                return 1
+                ;;
+        *)
+                ____indent="$3"
+                ;;
+        esac
+
+        printf -- "%s" "\
+$(views_shell_get_indent "$____indent")if ${2}; then
+" >> "${1}.tmp"
+        if [ $? -ne 0 ]; then
+                unset ____indent
+                return 1
+        fi
+        unset ____indent
+
+
+        # report status
+        return 0
+}
+
+
+
+
+# report import status
+return 0
