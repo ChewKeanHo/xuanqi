cf9ef4d72d8b51dcbba3d80bbb3f9848acdc672d
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-19 13:38:55 +0800

    XuanQi: added views_shell_write_import_guard_opener
    Since we need to create shell script systematically, we have
    to develop the shell rendering function. Fortunately, we got
    Hestia Project to back us up so the porting should be relatively
    easy. Hence, let's do this.
    
    This patch adds views_shell_write_import_guard_opener into
    XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ XuanQi/.internals/views/shell/write_import_guard_opener.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+# Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
+# You must comply with the license to use the content. Get the License at:
+#
+# https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+
+
+
+
+# Parameters:
+#       ____path_dest
+#               - OPTIONAL
+#               - The destination file to write into.
+#               - Optional since the function is unused.
+#       ____symbol
+#               - OPTIONAL
+#               - The base indent level in round number.
+#               - When empty, the indent level will set to '0' (no indent).
+# Outputs:
+#       Write to $____path_dest File
+#               - the rendered output written into file.
+#               - no action on error.
+# Returns:
+#       Return Code
+#               - '0' means ok; error otherwise.
+#               - always 0 since the function is unused.
+views_shell_write_import_guard_opener() {
+        #____path_dest="$1"
+        #____symbol="$2"
+
+
+        # shell does not need any import guard
+
+
+        # report status
+        return 0
+}
+
+
+
+
+# report import status
+return 0
