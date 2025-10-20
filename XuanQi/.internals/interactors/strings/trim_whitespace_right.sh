8081ce0af061e16640e9dd3e32de26a627b315a7
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-21 07:06:09 +0800

    XuanQi: added interactors_strings_trim_whitespace_right
    Since we need to develop the trim whitespace right API for
    strings processing, we have to develop its interactor function.
    Hence, let's do this.
    
    This patch adds interactors_strings_trim_whitespace_right in
    XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ XuanQi/.internals/interactors/strings/trim_whitespace_right.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+# Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 "Holloway" Chew, Kean Ho <kean.ho.chew@zoralab.com>
+# Copyright 2023 ZORALab Enterprise <tech@zoralab.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
+# You must comply with the license to use the content. Get the License at:
+#
+# https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+
+
+
+
+# Parameters:
+#       ____content
+#               - OPTIONAL
+#               - The string for trimming.
+# Outputs:
+#       String
+#               - The trimmed string.
+#               - Empty when given empty.
+# Returns:
+#       Return Code
+#               - '0' means ok; error otherwise.
+#               - Always 0.
+interactors_strings_trim_whitespace_right() {
+        #____content="$1"
+
+
+        # validate inputs
+        if [ "$1" = "" ]; then
+                printf -- ""
+                return 0
+        fi
+
+
+        # execute
+        entities_strings_trim_whitespace_right "$1"
+        if [ $? -ne 0 ]; then
+                return 0
+        fi
+
+
+        # report status
+        return 0
+}
+
+
+
+
+# report import status
+return 0
