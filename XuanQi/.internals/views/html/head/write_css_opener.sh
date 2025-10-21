6f74283530f13bc6bc98299395bed45c58f6dbdf
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-22 07:36:20 +0800

    XuanQi: added views_html_head_write_webscript_opener
    Since the late HestiaHUGO has a lot of great rendering
    function, we should port its views_html_head_write_webscript_opener
    in. Hence, let's do this.
    
    This patch ports views_html_head_write_webscript_opener
    into XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ XuanQi/.internals/views/html/head/write_css_opener.sh
@@ -0,0 +1,135 @@
+#!/bin/sh
+# Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 "Holloway" Chew, Kean Ho <kean.ho.chew@zoralab.com>
+# Copyright 2023 ZORALab Enterprise <tech@zoralab.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
+# You must comply with the license to use the content. Get the License at:
+#
+# https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+
+
+
+
+# Parameters:
+#       ____path_dest
+#               - COMPULSORY
+#               - the destination file to write into.
+#       ____source_url
+#               - OPTIONAL
+#               - for external url sourcing.
+#               - when left empty, the opener will use the
+#                 '<style>' css inlining (default).
+#               - when set, the opener will use the '<link>'
+#                 with this value and auto-close it.
+#       ____indent_level
+#               - OPTIONAL
+#               - indentation level in round numerical number.
+#               - '0' or empty means no default indentation.
+#       ____properties
+#               - OPTIONAL
+#               - the "key='value'" properties (e.g. id='...').
+#               - Multi-line values where each line is an entry.
+#               - Each entry **MUST** comply to the following
+#                 format:
+#                             '[KEY]: [VALUE]'
+#                 where:
+#                       - ': ' is the separating delimiter.
+#                       - [KEY] is the name of the property
+#                               (e.g. 'id').
+#                       - [VALUE] is the value of the property
+#                               (e.g. 'my-target-1').
+#               - You are responsible for the key:value's data
+#                 validity as this function only renders the
+#                 inputs.
+# Outputs:
+#       Write to $____path_dest File
+#               - the rendered output written into file.
+#               - no action on error.
+# Returns:
+#       Return Code
+#               - '0' means ok; error otherwise.
+#               - error on empty/invalid '$____path_dest'.
+#               - error on invalid '$____indent_level' (e.g. not a number).
+#               - error on bad execution.
+views_html_head_write_css_opener() {
+        #____path_dest="$1"
+        #____source_url="$2"
+        #____indent_level="$3"
+        #____properties="$4"
+
+
+        # validate inputs
+        if [ "$1" = "" ]; then
+                return 1
+        fi
+
+        if [ ! "${1%/*}" = "$1" ]; then
+                if [ -d "${1%/*}" ]; then
+                        : # accepted
+                elif [ -L "${1%/*}" ] &&
+                [ -d "$(readlink --canonicalize "$1")" ]; then
+                        : # accepted
+                else
+                        return 1
+                fi
+        fi
+
+        case "$3" in
+        "")
+                ;;
+        *[!0-9]*)
+                return 1
+                ;;
+        *)
+                if [ "$3" -lt 0 ]; then
+                        return 1
+                fi
+        esac
+
+
+        # execute
+        ## process url sourcing type if available
+        ____url=""
+        if [ ! "$2" = "" ]; then
+                printf -- "%s" "\
+$(\
+        views_html_render_element_opener "link" "$3" "\
+rel: stylesheet
+href: ${2}
+${4}
+"
+)
+" >> "${1}.tmp"
+                if [ $? -ne 0 ]; then
+                        return 1
+                fi
+        else
+                printf -- "%s" "\
+$(\
+        views_html_render_element_opener "style" "$3" "\
+${4}
+"
+)
+" >> "${1}.tmp"
+                if [ $? -ne 0 ]; then
+                        return 1
+                fi
+        fi
+
+
+        # report status
+        return 0
+}
+
+
+
+
+# report import status
+return 0
