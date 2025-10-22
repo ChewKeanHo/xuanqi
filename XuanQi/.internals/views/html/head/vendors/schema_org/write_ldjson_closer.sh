b5f341caeadb3015e02af5c0dd0f1def8a2e7146
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-23 07:21:11 +0800

    XuanQi: added views_html_head_vendors_schema_org_write_ldjson_closer
    Since the late HestiaHUGO has a lot of great rendering function,
    we should port views_html_head_vendors_schema_org_write_ldjson_closer
    into XuanQi. Hence, let's do this.
    
    This patch adds views_html_head_vendors_schema_org_write_ldjson_closer
    into XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ XuanQi/.internals/views/html/head/vendors/schema_org/write_ldjson_closer.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+# Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 "Holloway" Chew, Kean Ho <kean.ho.chew@zoralab.com>
+# Copyright 2023 ZORALab Enterprise <tech@zoralab.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
+# You must comply with the license to use the content. Get the License at:
+#
+# https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+
+
+
+
+# Specifications:
+#       * https://schema.org/
+# Parameters:
+#       ____path_dest
+#               - COMPULSORY
+#               - the destination file to write into.
+#       ____indent_level
+#               - OPTIONAL
+#               - indentation level in round numerical number.
+#               - empty means default '1' indentation.
+# Outputs:
+#       Write to $____path_dest File
+#               - the rendered output written into file.
+#               - no action on error.
+# Returns:
+#       Return Code
+#               - '0' means ok; error otherwise.
+#               - error on empty/invalid '$____path_dest'.
+#               - error on invalid '$____indent_level' (e.g. not a number).
+#               - error on bad execution.
+views_html_head_vendors_schema_org_write_ldjson_closer() {
+        #____path_dest="$1"
+        #____indent_level="$2"
+
+
+        # validate input
+        if [ "$1" = "" ]; then
+                return 1
+        fi
+
+        if [ "$2" = "" ]; then
+                return 1
+        fi
+
+
+        # execute
+        views_html_write_webscript_closer "$1" "${2:-1}"
+        if [ $? -ne 0 ]; then
+                return 1
+        fi
+
+
+        # report status
+        return 0
+}
+
+
+
+
+# report import status
+return 0
