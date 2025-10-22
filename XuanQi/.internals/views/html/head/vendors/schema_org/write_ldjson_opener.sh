bdf917c5bfbbab458c4aacabdbf9195ebc5d0152
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-23 06:46:11 +0800

    XuanQi: added views_html_head_vendors_schema_org_write_ldjson_opener
    Since the late HestiaHUGO has a lot of great rendering functions,
    let's port views_html_head_vendors_schema_org_write_ldjson_opener
    into here. Hence, let's do this.
    
    This patch adds views_html_head_vendors_schema_org_write_ldjson_opener
    into XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ XuanQi/.internals/views/html/head/vendors/schema_org/write_ldjson_opener.sh
@@ -0,0 +1,91 @@
+#!/bin/sh
+# Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 "Holloway" Chew, Kean Ho <kean.ho.chew@zoralab.com>
+# Copyright 2023 ZORALab Enterprise <tech@zoralab.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
+# You must comply with the license to use the content. Get the License at:
+#
+# https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+
+
+
+
+# Specifications:
+#       * https://schema.org/
+# Parameters:
+#       ____path_dest
+#               - COMPULSORY
+#               - the destination file to write into.
+#       ____properties
+#               - OPTIONAL
+#               - the "key='value'" properties (e.g. id='...').
+#               - multi-line values where each line is an entry.
+#               - the "type='application/ld+json' is already rendered
+#                 automatically so please exclude them accordingly.
+#               - each entry **MUST** comply to the following format:
+#                                   '[KEY]: [VALUE]'
+#                 where:
+#                       - ': ' is the separating delimiter.
+#                       - [KEY] is the name of the property
+#                               (e.g. 'id').
+#                       - [VALUE] is the value of the property
+#                               (e.g. 'my-target-1').
+#               - you are responsible for the key:value's data
+#                 validity as this function only renders the
+#                 inputs.
+#       ____indent_level
+#               - OPTIONAL
+#               - indentation level in round numerical number.
+#               - empty means default '1' indentation.
+# Outputs:
+#       Write to $____path_dest File
+#               - the rendered output written into file.
+#               - no action on error.
+# Returns:
+#       Return Code
+#               - '0' means ok; error otherwise.
+#               - error on empty/invalid '$____path_dest'.
+#               - error on invalid '$____indent_level' (e.g. not a number).
+#               - error on bad execution.
+views_html_head_vendors_schema_org_write_ldjson_opener() {
+        #____path_dest="$1"
+        #____properties="$2"
+        #____indent_level="$3"
+
+
+        # validate input
+        if [ "$1" = "" ]; then
+                return 1
+        fi
+
+        if [ "$2" = "" ]; then
+                return 1
+        fi
+
+
+        # execute
+        views_html_write_webscript_opener "$1" "sync" "" "${3:-1}" "\
+type: application/ld+json
+${2}
+"
+        if [ $? -ne 0 ]; then
+                return 1
+        fi
+
+
+        # report status
+        return 0
+}
+
+
+
+
+# report import status
+return 0
