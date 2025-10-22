30f15fed2a33e172fc5304d0d156e719d5bd8f49
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-22 09:05:53 +0800

    XuanQi: added views_html_head_write_relational_link
    Since the late HestiaHUGO has a lot of great rendering functions,
    we should port views_html_head_write_relational_link into here.
    Hence, let's do this.
    
    This patch adds views_html_head_write_relational_link into XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ XuanQi/.internals/views/html/head/write_relational_link.sh
@@ -0,0 +1,104 @@
+#!/bin/sh
+# Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 "Holloway" Chew, Kean Ho <kean.ho.chew@zoralab.com>
+# Copyright 2023 ZORALab Enterprise <tech@zoralab.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
+# You must comply with the license to use the content. Get the License at:
+#
+# https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+
+
+
+
+# Parameters:
+#       ____path_dest
+#               - COMPULSORY
+#               - the destination file to write into.
+#       ____relationship
+#               - COMPULSORY
+#               - the link relationship (e.g. 'stylesheet').
+#       ____source_url
+#               - COMPULSORY
+#               - for external url sourcing.
+#       ____properties
+#               - OPTIONAL
+#               - the "key='value'" properties (e.g. id='...').
+#               - Multi-line values where each line is an entry.
+#               - Each entry **MUST** comply to the following
+#                 format:
+#                             '[KEY]: [VALUE]'
+#                 where:
+#                       - ': ' is the separating delimiter.
+#                       - [KEY] is the name of the property
+#                               (e.g. 'id').
+#                       - [VALUE] is the value of the property
+#                               (e.g. 'my-target-1').
+#               - You are responsible for the key:value's data
+#                 validity as this function only renders the
+#                 inputs.
+#       ____indent_level
+#               - OPTIONAL
+#               - indentation level in round numerical number.
+#               - empty means default '1' indentation.
+# Outputs:
+#       Write to $____path_dest File
+#               - the rendered output written into file.
+#               - no action on error.
+# Returns:
+#       Return Code
+#               - '0' means ok; error otherwise.
+#               - error on empty/invalid '$____path_dest'.
+#               - error on invalid '$____indent_level' (e.g. not a number).
+#               - error on bad execution.
+views_html_head_write_relational_link() {
+        #____path_dest="$1"
+        #____relationship="$2"
+        #____source_url="$3"
+        #____properties="$4"
+        #____indent_level="$5"
+
+
+        # validate input
+        if [ "$1" = "" ]; then
+                return 1
+        fi
+
+        if [ "$2" = "" ]; then
+                return 1
+        fi
+
+        if [ "$3" = "" ]; then
+                return 1
+        fi
+
+
+        # execute
+        printf -- "%s" "\
+$(\
+        views_html_render_element_opener "link" "${5:-1}" "\
+rel: ${2}
+href: ${3}
+${4}
+")
+" >> "${1}.tmp"
+        if [ $? -ne 0 ]; then
+                return 1
+        fi
+
+
+        # report status
+        return 0
+}
+
+
+
+
+# report import status
+return 0
