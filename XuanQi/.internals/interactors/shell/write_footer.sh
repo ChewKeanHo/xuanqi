61d34acc5f1e887e0286fa66197fa62e44f1311a
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-24 07:54:01 +0800

    XuanQi: added interactors_shell_write_footer
    Since we need to render shell script, we have to enable their
    interactor rendering function. Hence, let's do this.
    
    This patch adds interactors_shell_write_footer into XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

--- XuanQi/.internals/interactors/shell/write_footer.sh
+++ XuanQi/.internals/interactors/shell/write_footer.sh
@@ -2,6 +2,8 @@
 # Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
 # Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
 # Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 "Holloway" Chew, Kean Ho <kean.ho.chew@zoralab.com>
+# Copyright 2023 ZORALab Enterprise <tech@zoralab.com>
 #
 #
 # Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
@@ -28,7 +30,7 @@
 #               - '0' means ok; error otherwise.
 #               - error on invalid '$____path_dest' (e.g. 'empty').
 #               - error on bad execution.
-views_shell_write_footer() {
+interactors_shell_write_footer() {
         #____path_dest="$1"
 
 
@@ -50,14 +52,7 @@ views_shell_write_footer() {
 
 
         # execute
-        printf -- "%s" "\
-
-
-
-
-# report import status
-return 0
-" >> "${1}.tmp"
+        views_shell_write_footer "$1"
         if [ $? -ne 0 ]; then
                 return 1
         fi
