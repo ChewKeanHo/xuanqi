607b68373c7760562d39616081bcd64b0ea06b15
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-23 14:54:19 +0800

    XuanQi: enabled help config section
    Since the help config interactor function is ready, let's enable
    it and off-load some of our knowledge into official documentations.
    Hence, let's do this.
    
    This patch enables help config section in XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

--- XuanQi/.internals/presenters/help/main.sh
+++ XuanQi/.internals/presenters/help/main.sh
@@ -28,6 +28,9 @@ interactors_print_info "$(interactors_print_ui_horizontal_line 3)"
 
 # run through each specific section
 case "$2" in
+config)
+        interactors_print_info "$(interactors_print_help_config_index "$3")"
+        ;;
 create)
         case "$3" in
         config)
