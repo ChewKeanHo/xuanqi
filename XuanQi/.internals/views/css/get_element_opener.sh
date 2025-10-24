e550a3cff7fc52887994a5f054b995b722bb5072
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-24 17:53:41 +0800

    XuanQi: added views_css_get_element_opener
    Since we need to render CSS file, we have to develop its
    rendering function. Let's start with its generic opener.
    
    This patch adds views_css_get_element_opener into XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ XuanQi/.internals/views/css/get_element_opener.sh
@@ -0,0 +1,111 @@
+#!/bin/sh
+# Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 "Holloway" Chew, Kean Ho <kean.ho.chew@zoralab.com>
+# Copyright 2023 ZORALab Enterprise <tech@zoralab.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
+# You must comply with the license to use the content. Get the License at:
+#
+# https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+
+
+
+
+# Specifications:
+#       - https://www.w3.org/Style/CSS/specs.en.html
+# Parameters:
+#       ____selectors
+#               - COMPULSORY
+#               - list of selectors.
+#               - Multi-line values where each line is a new entry.
+#               - Each entry does not need to have tailing comma (,).
+#       ____indent_level
+#               - OPTIONAL
+#               - indentation level in round numerical number.
+#               - '0' or empty means no default indentation.
+# Outputs:
+#       String
+#               - the rendered output string. Example:
+#                 "\
+#                 [Selector1],
+#                 [Selector2],
+#                 ...
+#                 [SelectorN] {
+#                 "
+# Returns:
+#       Return Code
+#               - '0' means ok; error otherwise.
+#               - error on empty '$___selectors'.
+#               - error on bad execution.
+views_css_get_element_opener() {
+        #____selectors="$1"
+        #____indent_level="$2"
+
+
+        # validate inputs
+        if [ "$1" = "" ]; then
+                return 1
+        fi
+
+        case "$2" in
+        "")
+                ;;
+        *[!0-9]*)
+                return 1
+                ;;
+        *)
+                if [ $2 -lt 0 ]; then
+                        return 1
+                fi
+                ;;
+        esac
+
+
+        # execute
+        ____output=""
+        ____old_IFS="$IFS"
+        while IFS="" read -r ____line || [ -n "$____line" ]; do
+                ____line="${____line%,}"
+                if [ "$____line" = "" ]; then
+                        continue
+                fi
+
+                if [ ! "$____output" = "" ]; then
+                        ____output="${____output},
+"
+                fi
+
+                ____output="${____output}$(views_css_get_indent "${2:-0}")${____line}"
+        done<<EOF
+${1}
+EOF
+        IFS="$____old_IFS"
+        unset ____line ____old_IFS
+        ____output="${____output} {
+"
+
+
+        # all good - write now
+        printf -- "%s" "$____output"
+        if [ $? -ne 0 ]; then
+                unset ____output
+                return 1
+        fi
+        unset ____output
+
+
+        # report status
+        return 0
+}
+
+
+
+
+# report import status
+return 0
