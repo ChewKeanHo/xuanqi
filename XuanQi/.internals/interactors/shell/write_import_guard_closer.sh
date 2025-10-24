e2b148ae88b443d203904f56e97776a7a37c23b7
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-24 11:00:27 +0800

    XuanQi: added interactors_shell_write_import_guard_closer
    Since we need to render shell script, we have to enable its
    interactor functions. Hence, let's deal with
    write_import_guard_closer.
    
    This patch adds interactors_shell_write_import_guard_closer
    into XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

--- XuanQi/.internals/interactors/shell/write_import_guard_closer.sh
+++ XuanQi/.internals/interactors/shell/write_import_guard_closer.sh
@@ -2,6 +2,8 @@
 # Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
 # Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
 # Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 "Holloway" Chew, Kean Ho <kean.ho.chew@zoralab.com>
+# Copyright 2023 ZORALab Enterprise <tech@zoralab.com>
 #
 #
 # Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
@@ -17,9 +19,8 @@
 
 # Parameters:
 #       ____path_dest
-#               - OPTIONAL
+#               - COMPULSORY
 #               - the destination file to write into.
-#               - optional since the function is unused.
 #       ____symbol
 #               - OPTIONAL
 #               - the name of the package library.
@@ -30,14 +31,39 @@
 # Returns:
 #       Return Code
 #               - '0' means ok; error otherwise.
-#               - always 0 since the function is unused.
-views_shell_write_import_guard_closer() {
+#               - error on empty/invalid '$____path_dest'.
+#               - error on bad execution.
+interactors_shell_write_import_guard_closer() {
         #____path_dest="$1"
         #____symbol="$2"
 
 
+        # validate inputs
+        if [ "$1" = "" ]; then
+                return 1
+        fi
+
+        if [ ! "${1%/*}" = "$1" ]; then
+                if [ -d "${1%/*}" ]; then
+                        : # accepted
+                elif [ -L "${1%/*}" ] &&
+                [ -d "$(readlink --canonicalize "$1")" ]; then
+                        : # accepted
+                else
+                        return 1
+                fi
+        fi
+
+        if [ "$2" = "" ]; then
+                return 0
+        fi
+
+
         # execute
-        ## Shell does not need any import guard
+        views_shell_write_import_guard_closer "$1" "$2"
+        if [ $? -ne 0 ]; then
+                return 1
+        fi
 
 
         # report status
