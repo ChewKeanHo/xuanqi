26499f19f0614af4b1d0357620676118f91f2712
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-24 11:27:04 +0800

    XuanQi: added interactors_shell_write_long_string_content
    Since we need to render shell script, we have to enable its
    interactor functions. Hence, let's deal with write_long_string_content.
    
    This patch adds interactors_shell_write_long_string_content into
    XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

--- XuanQi/.internals/interactors/shell/write_long_string_content.sh
+++ XuanQi/.internals/interactors/shell/write_long_string_content.sh
@@ -2,6 +2,8 @@
 # Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
 # Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
 # Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 "Holloway" Chew, Kean Ho <kean.ho.chew@zoralab.com>
+# Copyright 2023 ZORALab Enterprise <tech@zoralab.com>
 #
 #
 # Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
@@ -33,7 +35,7 @@
 #               - '0' means ok; error otherwise.
 #               - error on empty/invalid '$____path_dest'.
 #               - error on bad execution.
-views_shell_write_long_string_content() {
+interactors_shell_write_long_string_content() {
         #____path_dest="$1"
         #____content="$2"
 
@@ -60,41 +62,10 @@ views_shell_write_long_string_content() {
 
 
         # execute
-        ____old_IFS="$IFS"
-        while IFS= read -r ____line || [ -n "$____line" ]; do
-                ____output=""
-                while [ -n "$____line" ]; do
-                        ____char="${____line%"${____line#?}"}"
-                        ____line="${____line#?}"
-
-                        ## escape functional characters
-                        case "$____char" in
-                        "\\")
-                                ____output="${____output}\\\\"
-                                ;;
-                        "\"")
-                                ____output="${____output}\\\""
-                                ;;
-                        *)
-                                ____output="${____output}${____char}"
-                                ;;
-                        esac
-                done
-
-                ## all good - write now
-                printf -- "%s" "\
-${____output}
-" >> "${1}.tmp"
-                if [ $? -ne 0 ]; then
-                        IFS="$____old_IFS"
-                        unset ____char ____output ____line  ____old_IFS
-                        return 1
-                fi
-        done<<EOF
-${2}
-EOF
-        IFS="$____old_IFS"
-        unset ____char ____output ____line  ____old_IFS
+        views_shell_write_long_string_content "$1" "$2"
+        if [ $? -ne 0 ]; then
+                return 1
+        fi
 
 
         # report status
