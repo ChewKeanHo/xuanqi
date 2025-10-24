f3d41c728028ee3200862135a232da4f6be8260a
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-24 16:23:17 +0800

    XuanQi: added interactors_print_components_init_data
    Since we have the init component data's documentation data
    source ready, we can enable its interactor function. Hence,
    let's do this.
    
    This patch adds interactors_print_components_init_data into
    XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ XuanQi/.internals/interactors/print/components/init_data.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+# Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 "Holloway" Chew, Kean Ho <kean.ho.chew@zoralab.com>
+# Copyright 2023 ZORALab Enterprise <tech@zoralab.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
+# You must comply with the license to use the content. Get the License at:
+#
+# https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+
+
+
+
+# Parameters:
+# Outputs:
+#       String
+#               - the data content.
+#               - empty on error.
+# Returns:
+#       Return Code
+#               - '0' means ok; error otherwise.
+#               - error when internal file is missing.
+#               - error when internal file is unreadable.
+interactors_print_components_init_data() {
+        # validate inputs
+        ____path="\
+${XUANQI_PATH_ROOT}/entities/docs/components/init_data/${XUANQI_LANGUAGE}.txt\
+"
+        if [ ! -f "$____path" ]; then
+                # fallback to english
+                ____path="\
+${XUANQI_PATH_ROOT}/entities/docs/components/init_data//en.txt\
+"
+        fi
+
+
+        # execute
+        entities_fs_read_text_file "$____path"
+        if [ $? -ne 0 ]; then
+                unset ____path
+                return 1
+        fi
+        unset ____path
+
+
+        # report status
+        return 0
+}
+
+
+
+
+# report import status
+return 0
