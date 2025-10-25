e073f05d002ef7cd0076e8881d9d756ea2b20d9f
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-25 12:08:24 +0800

    XuanQi: added interactors_css_get_element_closer
    Since we have views_css_get_element_closer data rendering function
    ready, we can proceed to enable its interactor function. Hence, let's
    do this.
    
    This patch adds interactors_css_get_element_closer in XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ XuanQi/.internals/interactors/css/get_element_closer.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+# Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 "Holloway" Chew, Kean Ho <kean.ho.chew@zoralab.com>
+# Copyright 2023 ZORALab Enterprise <tech@zoralab.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
+# You must comply with the license to use the content. Get the License at:
+#
+# https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+
+
+
+
+# Specifications:
+#       - https://www.w3.org/Style/CSS/specs.en.html
+# Parameters:
+#       ____indent_level
+#               - OPTIONAL
+#               - indentation level in round numerical number.
+#               - '0' or empty means no default indentation.
+# Outputs:
+#       String
+#               - the rendered output string. Example: "}".
+# Returns:
+#       Return Code
+#               - '0' means ok; error otherwise.
+#               - error on invalid '$____indent_level' (e.g. not a number).
+#               - error on bad execution.
+interactors_css_get_element_closer() {
+        #____indent_level="$1"
+
+
+        # validate inputs
+        case "$1" in
+        "")
+                ;;
+        *[!0-9]*)
+                return 1
+                ;;
+        *)
+                if [ $1 -lt 0 ]; then
+                        return 1
+                fi
+                ;;
+        esac
+
+
+        # execute
+        views_css_get_element_closer "${1:-0}"
+        if [ $? -ne 0 ]; then
+                return 1
+        fi
+
+
+        # report status
+        return 0
+}
+
+
+
+
+# report import status
+return 0
