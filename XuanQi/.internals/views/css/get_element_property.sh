5859ed2c09516e2c025d0b50863fc7174665e30a
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-25 09:02:56 +0800

    XuanQi: added views_css_get_element_property
    Since we need a way to render a CSS porperty, we have to develop
    its rendering function. Hence, let's do this.
    
    This patch adds views_css_get_element_property into XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ XuanQi/.internals/views/css/get_element_property.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+# Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 "Holloway" Chew, Kean Ho <kean.ho.chew@zoralab.com>
+# Copyright 2023 ZORALab Enterprise <tech@zoralab.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
+# You must comply with the license to use the content. Get the License at:
+#
+# https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+
+
+
+
+# Specifications:
+#       - https://www.w3.org/Style/CSS/specs.en.html
+# Parameters:
+#       ____name
+#               - COMPULSORY
+#               - property name (e.g. 'margin').
+#       ____value
+#               - COMPULSORY
+#               - property value (e.g. '1rem').
+#       ____indent_level
+#               - OPTIONAL
+#               - indentation level in round numerical number.
+#               - '0' or empty means no default indentation.
+# Outputs:
+#       String
+#               - the rendered output string. Example: "margin: 1rem;".
+# Returns:
+#       Return Code
+#               - '0' means ok; error otherwise.
+#               - error on empty '$____name'.
+#               - error on empty '$____value'.
+#               - error on invalid '$____indent' (e.g. not a number).
+#               - error on bad execution.
+views_css_get_element_property() {
+        #____selectors="$1"
+        #____indent_level="$2"
+
+
+        # validate inputs
+        if [ "$1" = "" ]; then
+                return 1
+        fi
+
+        if [ "$2" = "" ]; then
+                return 1
+        fi
+
+        case "$3" in
+        "")
+                ;;
+        *[!0-9]*)
+                return 1
+                ;;
+        *)
+                if [ $3 -lt 0 ]; then
+                        return 1
+                fi
+                ;;
+        esac
+
+
+        # execute
+        printf -- "%s" "\
+$(views_css_get_indent "${3:-0}")${1}: ${2};
+"
+
+
+        # report status
+        return 0
+}
+
+
+
+
+# report import status
+return 0
