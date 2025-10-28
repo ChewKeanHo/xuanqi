f796de0f5904f5ab9ff264e5232a29fd1b61da58
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-28 13:18:43 +0800

    XuanQi: added interactors_css_write_header
    Since we have the css header rendering data function ready,
    let's enable its interactor function. Hence, let's do this.
    
    This patch adds interactors_css_write_header in XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ XuanQi/.internals/interactors/css/write_header.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+# Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 "Holloway" Chew, Kean Ho <kean.ho.chew@zoralab.com>
+# Copyright 2023 ZORALab Enterprise <tech@zoralab.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
+# You must comply with the license to use the content. Get the License at:
+#
+# https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+
+
+
+
+# Parameters:
+#       ____path_dest
+#               - COMPULSORY
+#               - the destination file to write into.
+#       ____license_holders
+#               - OPTIONAL
+#               - license copyright entries.
+#               - multiline values where each line is an entry.
+#               - no need to prefix '#' comment syntax.
+#               - recommended entry format:
+#                  'Copyright [YEAR] [AUTHOR] [CONTACT (email/url)]'
+#               - when left empty, a copyright holder sample is used.
+# Outputs:
+#       Write to '$____path_dest' File
+#               - the rendered output written into file.
+#               - no action on error.
+# Returns:
+#       Return Code
+#               - '0' means ok; error otherwise.
+#               - error on empty/invalid '$____path_dest'.
+#               - error on bad execution.
+interactors_css_write_header() {
+        #____path_dest="$1"
+        #____license_holders="$2"
+
+
+        # validate inputs
+        if [ "$1" = "" ]; then
+                return 1
+        fi
+
+        if [ ! "${1%/*}" = "$1" ]; then
+                if [ -d "${1%/*}" ]; then
+                        : # accepted
+                elif [ -L "${1%/*}" ] &&
+                [ -d "$(readlink --canonicalize "$1")" ]; then
+                        : # accepted
+                else
+                        return 1
+                fi
+        fi
+
+
+        # execute
+        views_css_write_header \
+                "$1" \
+                "${2:-"$(interactors_print_license_sample_copyright_holder)"}" \
+                "$(interactors_print_license_notice)"
+        if [ $? -ne 0 ]; then
+                return 1
+        fi
+
+
+        # report status
+        return 0
+}
+
+
+
+
+# report import status
+return 0
