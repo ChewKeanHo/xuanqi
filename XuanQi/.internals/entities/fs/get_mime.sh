ad4caef6926857d4cff662ff03c1016b21ee772f
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-28 10:28:17 +0800

    XuanQi: added entities_fs_get_mime
    We need a reliable way to obtain a file's mime. Fortunately,
    on UNIX system, the 'file' program get things done seamlessly
    so we just have to integrate it. Hence, let's do this.
    
    This patch adds entities_fs_get_mime into XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ XuanQi/.internals/entities/fs/get_mime.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+# Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 "Holloway" Chew, Kean Ho <kean.ho.chew@zoralab.com>
+# Copyright 2023 ZORALab Enterprise <tech@zoralab.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
+# You must comply with the license to use the content. Get the License at:
+#
+# https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+
+
+
+
+# Specifications:
+#       - https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/MIME_types
+# Parameters:
+#       ____path
+#               - REQUIRED
+#               - The target path for analysis.
+# Outputs:
+#       MIME string
+#               - network-compatible MIME value.
+#               - empty on error.
+# Returns:
+#       Return Code
+#               - '0' means ok; error otherwise.
+#               - error on empty any value.
+#               - error on missing '$____path'.
+#               - error on missing 'file' command.
+#               - error on bad execution.
+entities_fs_get_mime() {
+        #____path="$1"
+
+
+        # validate inputs
+        if [ "$1" = "" ]; then
+                printf -- ""
+                return 1
+        fi
+
+        if [ ! -e "$1" ]; then
+                printf -- ""
+                return 1
+        fi
+
+        command -v file 2> /dev/null
+        if [ $? -ne 0 ]; then
+                printf -- ""
+                return 1
+        fi
+
+
+        # execute
+        ____output="$(file --brief --mime-type "$1" 2> /dev/null)"
+        case "$____output" in
+        ""|*cannot*)
+                printf -- ""
+
+                unset ____output
+                return 1
+                ;;
+        *)
+                printf -- "%s" "$____output"
+                if [ $? -ne 0 ]; then
+                        unset ____output
+                        return 1
+                fi
+
+                unset ____output
+                ;;
+        esac
+
+
+        # report status
+        return 0
+}
+
+
+
+
+# report import status
+return 0
