ff363e021ed2d9cd33e9cc60c684d7e96109ab54
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-30 07:56:33 +0800

    XuanQi: added XuanQi_CSS_Write_Raw_Content API
    Since we need the public CSS rendering function, we should now
    publish its APIs. Let's deal with Write_Raw_Content.
    
    This patch adds XuanQi_CSS_Write_Raw_Content API into XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ XuanQi/.internals/API/CSS/Write_Raw_Content.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+# Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 "Holloway" Chew, Kean Ho <kean.ho.chew@zoralab.com>
+# Copyright 2023 ZORALab Enterprise <tech@zoralab.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
+# You must comply with the license to use the content. Get the License at:
+#
+# https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+
+
+
+
+# Specifications:
+#       - Designed for writing CSS raw content directory from shell.
+#       - Allows shell's variable and logic uses for percision rendering.
+#       - You have total control over what to render into CSS.
+# Parameters:
+#       ____path_dest
+#               - COMPULSORY
+#               - The destination file to write into.
+#               - Resolve symlink.
+#       ____content
+#               - OPTIONAL
+#               - The raw content to be written as it is.
+#               - Empty content does nothing and return early.
+# Outputs:
+#       Write to '$____path_dest' File
+#               - The rendered output written into file.
+#               - No action on error.
+# Returns:
+#       Return Code
+#               - '0' means ok; error otherwise.
+#               - Error on empty/invalid '$____path_dest'.
+#               - Error on bad execution.
+XuanQi_CSS_Write_Raw_Content() {
+        #____path_dest="$1"
+        #____content="$2"
+
+
+        # validate inputs
+        if [ "$1" = "" ]; then
+                return 1
+        fi
+
+        if [ ! "${1%/*}" = "$1" ]; then
+                if [ -d "${1%/*}" ]; then
+                        : # accepted
+                elif [ -L "${1%/*}" ] &&
+                [ -d "$(readlink --canonicalize "$1")" ]; then
+                        : # accepted
+                else
+                        return 1
+                fi
+        fi
+
+        if [ "$2" = "" ]; then
+                return 0
+        fi
+
+
+        # execute
+        interactors_css_write_raw_content "$1" "$2"
+        if [ $? -ne 0 ]; then
+                return 1
+        fi
+
+
+        # report status
+        return 0
+}
+
+
+
+
+# report import status
+return 0
