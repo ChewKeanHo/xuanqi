4be2bead42f1cf982114672c1cae5b0797a8c708
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2025-10-30 08:39:54 +0800

    XuanQi: added XuanQi_CSS_Write_Opener API
    Since we need the CSS rendering APIs at the public side,
    we must publish them. Hence, let's deal with Write_Opener.
    
    This patch adds XuanQi_CSS_Write_Opener API into XuanQi.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: 湘渝萱 <alice.xiang.yu.xuan@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Reviewed-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ XuanQi/.internals/API/CSS/Write_Opener.sh
@@ -0,0 +1,110 @@
+#!/bin/sh
+# Copyright 2025 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+# Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 "Holloway" Chew, Kean Ho <kean.ho.chew@zoralab.com>
+# Copyright 2023 ZORALab Enterprise <tech@zoralab.com>
+#
+#
+# Licensed under (Holloway) Chew, Kean Ho's Liberal License (the 'License').
+# You must comply with the license to use the content. Get the License at:
+#
+# https://doi.org/10.5281/zenodo.13770769
+#
+# You MUST ensure any interaction with the content STRICTLY COMPLIES with
+# the permissions and limitations set forth in the license.
+
+
+
+
+# Specifications:
+#       - Designed to create an opener (e.g. 'media ... {' or 'body {')
+#         reliably.
+#       - Programmable nature enables continuous improvement.
+#       - Learn more at: https://www.w3.org/Style/CSS/specs.en.html
+# Parameters:
+#       ____path_dest
+#               - COMPULSORY
+#               - The destination file to write into.
+#               - Resolve symlink.
+#       ____selectors
+#               - COMPULSORY
+#               - List of selectors.
+#               - Multi-line values where each line is a new entry.
+#               - Each entry does not need to have tailing comma (,).
+#       ____indent_level
+#               - OPTIONAL
+#               - Indentation level in round numerical number.
+#               - '0' or empty means no default indentation.
+# Outputs:
+#       String
+#               - The rendered output string. Example:
+#                 "\
+#                 [Selector1],
+#                 [Selector2],
+#                 ...
+#                 [SelectorN] {
+#                 "
+# Returns:
+#       Return Code
+#               - '0' means ok; error otherwise.
+#               - Error on empty/invalid '$____path_dest'.
+#               - Error on empty '$____selectors'.
+#               - Error on invalid '$____indent' (e.g. not a number).
+#               - Error on bad execution.
+XuanQi_CSS_Write_Opener() {
+        #____path_dest="$1"
+        #____selectors="$2"
+        #____indent_level="$3"
+
+
+        # validate inputs
+        if [ "$1" = "" ]; then
+                return 1
+        fi
+
+        if [ ! "${1%/*}" = "$1" ]; then
+                if [ -d "${1%/*}" ]; then
+                        : # accepted
+                elif [ -L "${1%/*}" ] &&
+                [ -d "$(readlink --canonicalize "$1")" ]; then
+                        : # accepted
+                else
+                        return 1
+                fi
+        fi
+
+        if [ "$2" = "" ]; then
+                return 1
+        fi
+
+        case "$3" in
+        "")
+                ;;
+        *[!0-9]*)
+                return 1
+                ;;
+        *)
+                if [ $3 -lt 0 ]; then
+                        return 1
+                fi
+                ;;
+        esac
+
+
+        # execute
+        interactors_css_write_opener "$1" "$2" "$3"
+        if [ $? -ne 0 ]; then
+                return 1
+        fi
+
+
+        # report status
+        return 0
+}
+
+
+
+
+# report import status
+return 0
